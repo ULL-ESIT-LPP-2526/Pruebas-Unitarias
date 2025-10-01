3330eee47fff2a910c3278505009c985b0624df8
Coromoto León <cleon@ull.edu.es>
2025-10-01 09:23:35 +0000

    modificaciones en la clase

--- ej/ejemplo.rb
+++ ej/ejemplo.rb
@@ -2,9 +2,11 @@ require 'test/unit'
 
 class MyTest < Test::Unit::TestCase
   # def setup
+  # El código que se ejecuta antes de todas la pruebas
   # end
 
   # def teardown
+  # El código que se ejecuta al finalizar el banco de pruebas
   # end
 
   def test_fail
